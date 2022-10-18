d366dccea536a9d92d3974c9e582531c64006893
Vladislav Proskurov <rilshok@pm.me>
2022-10-18 17:10:05 +0300

    + Labels

--- bimcvcovid19i/typing.py
+++ bimcvcovid19i/typing.py
@@ -19,3 +19,21 @@ class Session:
     session_id: str
     study_date: str
     medical_evaluation: str
+
+
+@dataclass
+class Labels:
+    subject_id: str
+    session_id: str
+    report: str
+    labels: tp.List[str]
+    localizations: tp.List[str]
+    labels_localizations_by_sentence: tp.List[str]
+    label_CUIS: tp.List[str]
+    localizations_CUIS: tp.List[str]
+
+    def to_dict(self):
+        state = self.__dict__.copy()
+        state.pop("subject_id")
+        state.pop("session_id")
+        return state
