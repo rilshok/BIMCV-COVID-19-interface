802b189ad178c1a2a4f7a5577ba8584b37b242c1
Vladislav Proskurov <rilshok@pm.me>
2022-10-18 17:05:59 +0300

    + base dataclasses

--- bimcvcovid19i/typing.py
+++ bimcvcovid19i/typing.py
@@ -1,4 +1,21 @@
 import typing as tp
 from pathlib import Path
+from dataclasses import dataclass
 
 LikePath = tp.Union[str, Path]
+
+
+@dataclass
+class Subject:
+    subject_id: str
+    modalities: tp.List[str]
+    age: float
+    gender: str
+
+
+@dataclass
+class Session:
+    subject_id: str
+    session_id: str
+    study_date: str
+    medical_evaluation: str
