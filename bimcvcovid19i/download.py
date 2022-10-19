9d99245c9029928aaac7640a838d096e56915824
Vladislav Proskurov <rilshok@pm.me>
2022-10-19 09:18:51 +0300

    use BIMCVCOVID19Root instead of DatasetRoot

--- bimcvcovid19i/download.py
+++ bimcvcovid19i/download.py
@@ -1,10 +1,10 @@
-from .typing import DatasetRoot, LikePath
+from .typing import BIMCVCOVID19Root, LikePath
 from .webdav import webdav_download_all
 
 
 def download_bimcv_covid19_positive(root: LikePath):
     return webdav_download_all(
-        root=DatasetRoot(root).original,
+        root=BIMCVCOVID19Root(root).original,
         webdav_hostname="https://b2drop.bsc.es/public.php/webdav",
         webdav_login="BIMCV-COVID19-cIter_1_2",
         webdav_password="maybeempty",
@@ -13,7 +13,7 @@ def download_bimcv_covid19_positive(root: LikePath):
 
 def download_bimcv_covid19_negative(root: LikePath):
     return webdav_download_all(
-        root=DatasetRoot(root).original,
+        root=BIMCVCOVID19Root(root).original,
         webdav_hostname="https://b2drop.bsc.es/public.php/webdav",
         webdav_login="BIMCV-COVID19-cIter_1_2-Negative",
         webdav_password="maybeempty",
@@ -21,6 +21,6 @@ def download_bimcv_covid19_negative(root: LikePath):
 
 
 def extract_bimcv_covid19_positive(root: LikePath):
-    dsroot = DatasetRoot(root)
+    dsroot = BIMCVCOVID19Root(root)
     assert dsroot.original.exists()
     # TODO: check sums
