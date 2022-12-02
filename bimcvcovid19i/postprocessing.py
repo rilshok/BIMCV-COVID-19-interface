603222e26b5652e83cb53e74c3a3b758d8425e4a
Vladislav Proskurov <rilshok@pm.me>
2022-12-02 16:17:29 +0300

    different types of rotating transformations

---
+++ bimcvcovid19i/postprocessing.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+class Transform:
+    def __call__(self, image, spacing):
+        img = self.transform_image(image)
+        spc = self.transform_spacing(spacing)
+        return img, spc
+
+    def transform_image(self, image):
+        return image
+
+    def transform_spacing(self, spacing):
+        return spacing
+
+
+class RotateCTTransform(Transform):
+    pass
+
+
+class RotateCTTransformType0(RotateCTTransform):
+    pass
+
+
+class RotateCTTransformType1(RotateCTTransform):
+    def transform_image(self, image):
+        return np.rot90(np.rot90(image, 1, (1, 2)), 1)[..., ::-1]
+
+    def transform_spacing(self, spacing):
+        return spacing[2], spacing[0], spacing[1]
+
+
+class RotateCTTransformType2(RotateCTTransform):
+    def transform_image(self, image):
+        return np.rot90(image, 1, (1, 2))[..., ::-1]
+
+    def transform_spacing(self, spacing):
+        return spacing[0], spacing[2], spacing[1]
+
+
+class RotateCTTransformType3(RotateCTTransform):
+    def transform_image(self, image):
+        return np.rot90(image, 1, (0, 1))[..., ::-1]
+
+    def transform_spacing(self, spacing):
+        return spacing[1], spacing[0], spacing[2]
+
+
+class RotateCTTransformType4(RotateCTTransform):
+    def transform_image(self, image):
+        return np.rot90(image, 1, (1, 2))[..., ::-1, ::-1]
+
+    def transform_spacing(self, spacing):
+        return spacing[0], spacing[2], spacing[1]
+
+
+class RotateCTTransformType5(RotateCTTransform):
+    def transform_image(self, image):
+        return np.rot90(image, 1, (0, 1))
+
+    def transform_spacing(self, spacing):
+        return spacing[1], spacing[0], spacing[2]
