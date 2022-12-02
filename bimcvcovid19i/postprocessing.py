7c9afe632f302a61992f2f6667d436f366f036ec
Vladislav Proskurov <rilshok@pm.me>
2022-12-02 20:54:34 +0300

    type hints

--- bimcvcovid19i/postprocessing.py
+++ bimcvcovid19i/postprocessing.py
@@ -4,29 +4,33 @@ __all__ = [
 ]
 
 import contextlib
+from typing import Tuple
 
 import numpy as np
 from scipy.ndimage import maximum_filter, minimum_filter
 
 from .assets import mapping_bimcv_covid19_ct_rotate_transforms
 
+Image = np.ndarray
+Spacing = Tuple[float, ...]
 
-def assert_ndim(data, dim):
+
+def assert_ndim(data: Image, dim: int):
     if data.ndim != dim:
         msg = f"expected {dim} axes"
         raise ValueError(msg)
 
 
 class Transform:
-    def __call__(self, image, spacing):
+    def __call__(self, image: Image, spacing: Spacing):
         img = self.transform_image(image)
         spc = self.transform_spacing(spacing)
         return img, spc
 
-    def transform_image(self, image):
+    def transform_image(self, image: Image):
         return image
 
-    def transform_spacing(self, spacing):
+    def transform_spacing(self, spacing: Spacing):
         return spacing
 
 
@@ -39,42 +43,42 @@ class RotateCTTransformType0(RotateCTTransform):
 
 
 class RotateCTTransformType1(RotateCTTransform):
-    def transform_image(self, image):
+    def transform_image(self, image: Image):
         return np.rot90(np.rot90(image, 1, (1, 2)), 1)[..., ::-1]
 
-    def transform_spacing(self, spacing):
+    def transform_spacing(self, spacing: Spacing):
         return spacing[2], spacing[0], spacing[1]
 
 
 class RotateCTTransformType2(RotateCTTransform):
-    def transform_image(self, image):
+    def transform_image(self, image: Image):
         return np.rot90(image, 1, (1, 2))[..., ::-1]
 
-    def transform_spacing(self, spacing):
+    def transform_spacing(self, spacing: Spacing):
         return spacing[0], spacing[2], spacing[1]
 
 
 class RotateCTTransformType3(RotateCTTransform):
-    def transform_image(self, image):
+    def transform_image(self, image: Image):
         return np.rot90(image, 1, (0, 1))[..., ::-1]
 
-    def transform_spacing(self, spacing):
+    def transform_spacing(self, spacing: Spacing):
         return spacing[1], spacing[0], spacing[2]
 
 
 class RotateCTTransformType4(RotateCTTransform):
-    def transform_image(self, image):
+    def transform_image(self, image: Image):
         return np.rot90(image, 1, (1, 2))[..., ::-1, ::-1]
 
-    def transform_spacing(self, spacing):
+    def transform_spacing(self, spacing: Spacing):
         return spacing[0], spacing[2], spacing[1]
 
 
 class RotateCTTransformType5(RotateCTTransform):
-    def transform_image(self, image):
+    def transform_image(self, image: Image):
         return np.rot90(image, 1, (0, 1))
 
-    def transform_spacing(self, spacing):
+    def transform_spacing(self, spacing: Spacing):
         return spacing[1], spacing[0], spacing[2]
 
 
@@ -89,11 +93,11 @@ def get_rotate_ct_transform(transform_type: str) -> RotateCTTransform:
     )[transform_type]()
 
 
-def rotate_ct_transform(image, spacing, transform_type: str):
+def rotate_ct_transform(image: Image, spacing: Spacing, transform_type: str):
     return get_rotate_ct_transform(transform_type)(image, spacing)
 
 
-def _clean_3dimage(image, filter_fn):
+def _clean_3dimage(image: Image, filter_fn):
     assert_ndim(image, 3)
     skip = slice(None)
     first = slice(1, None)
@@ -117,19 +121,19 @@ def _clean_3dimage(image, filter_fn):
     return image
 
 
-def _blank_filter(image):
+def _blank_filter(image: Image):
     assert_ndim(image, 2)
     return len(np.unique(image)) == 1
 
 
-def _estimate_regularity(img, k: int = 10) -> float:
-    assert_ndim(img, 2)
-    min_, max_ = np.quantile(img, 0.03), np.quantile(img, 0.97)
+def _estimate_regularity(image: Image, k: int = 10) -> float:
+    assert_ndim(image, 2)
+    min_, max_ = np.quantile(image, 0.03), np.quantile(image, 0.97)
     with contextlib.suppress(Exception):
         if max_ - min_ <= 0:
             raise ValueError()
-        one = (img.mean(0) - min_) / (max_ - min_)
-        two = (img.mean(1) - min_) / (max_ - min_)
+        one = (image.mean(0) - min_) / (max_ - min_)
+        two = (image.mean(1) - min_) / (max_ - min_)
         minimum = np.concatenate(
             tuple(minimum_filter(vec, size=k) for vec in [one, two])
         )
@@ -140,22 +144,22 @@ def _estimate_regularity(img, k: int = 10) -> float:
     return 0.0
 
 
-def _regularity_filter(image):
+def _regularity_filter(image: Image):
     return _estimate_regularity(image) > 0.1
 
 
-def _blank_and_regularity_filter(image):
+def _blank_and_regularity_filter(image: Image):
     return _blank_filter(image) or _regularity_filter(image)
 
 
-def clean_ct_image(image):
+def clean_ct_image(image: Image):
     return _clean_3dimage(image, _blank_and_regularity_filter)
 
 
-def process_ct_image(uid, image, spacing):
-    if not uid.endswith("ct"):
+def process_ct_image(series_id: str, image: Image, spacing: Spacing):
+    if not series_id.endswith("ct"):
         raise ValueError("CT image identifier was expected")
-    transform_type = mapping_bimcv_covid19_ct_rotate_transforms().get(uid)
+    transform_type = mapping_bimcv_covid19_ct_rotate_transforms().get(series_id)
     if transform_type:
         image, spacing = rotate_ct_transform(
             image=image, spacing=spacing, transform_type=transform_type
